0d0c88aaf6256b296113f7077c20b4be5e29de3a
ardaozcelk <ardaozcelk_@hotmail.com>
2024-10-06 19:16:51 +0300

    design-home-page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
+import CartTotals from "./components/cart/CartTotals";
+import Categories from "./components/categories/Categories";
 import Header from "./components/header/Header";
+import Products from "./components/products/Products";
 
 function App() {
   return (
-    <div>
-      <Header/>
-      home
-    </div>
+    <>
+      <Header />
+      <div className="home px-6 flex justify-between gap-10">
+        <div className="categories overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
+          <Categories/>
+        </div>
+        <div className="products flex-[8]">
+          <Products/>
+        </div>
+        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
+          <CartTotals/>
+        </div>
+      </div>
+    </>
   );
 }
 
