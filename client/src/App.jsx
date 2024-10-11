65192c458282d19ce343bdb442b472d50ee395e5
ardaozcelk <ardaozcelk_@hotmail.com>
2024-10-11 15:10:48 +0300

    customerPage-responsiveBill

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
 import BillPages from "./pages/BillPages";
+import CustomerPage from "./pages/CustomerPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/bills" element={<BillPages />} />
+        <Route path="/customers" element={<CustomerPage />} />
 
       </Routes>
     </BrowserRouter>
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -42,7 +42,7 @@ const Header = () => {
                         <CopyOutlined className="md:text-2xl text-xl" />
                         <span className="md:text-xs text-[10px]">Faturalar</span>
                     </Link>
-                    <Link to={"/"} className="menu-link flex flex-col items-center justify-center space-y-1 hover:text-[#40a9ff] transition-all">
+                    <Link to={"/customers"} className="menu-link flex flex-col items-center justify-center space-y-1 hover:text-[#40a9ff] transition-all">
                         <UserOutlined className="md:text-2xl text-xl" />
                         <span className="md:text-xs text-[10px]">Müşteriler</span>
                     </Link>
