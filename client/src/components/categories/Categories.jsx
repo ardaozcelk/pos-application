f1fe6df858686661de8b00291ec1d7e207da8e9f
ardaozcelk <ardaozcelk_@hotmail.com>
2024-10-19 00:51:07 +0300

    category-component-fix

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,27 +1,11 @@
-import { PlusOutlined } from "@ant-design/icons";
+import { EditOutlined, PlusOutlined } from "@ant-design/icons";
 import { useState } from 'react';
-import { Button, Form, Input, Modal, message } from 'antd';
-import "./style.css"
-
+import Add from "./Add";
+import Edit from "./Edit";
+import "./style.css";
 const Categories = ({ categories, setCategories }) => {
     const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-
-    const [form] = Form.useForm()
-
-    const onFinish = (values) => {
-        try {
-            fetch("http://localhost:5000/api/categories/add-category", {
-                method: "POST",
-                body: JSON.stringify(values),
-                headers: { "Content-type": "application/json; charset=UTF-8" }
-            });
-            message.success("Kategori Başarıyla Eklendi!");
-            form.resetFields();
-            setCategories([...categories, values]); //tek satırdaki işlem ile anlık olarak yeni kategorileri görebiliriz. Sayfayı yenilemeden !!
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
     return (
         <ul className="flex gap-4 md:flex-col text-lg">
@@ -35,16 +19,12 @@ const Categories = ({ categories, setCategories }) => {
             <li className="category-item !bg-purple-800 hover:opacity-90" onClick={() => setIsAddModalOpen(true)}>
                 <PlusOutlined />
             </li>
-            <Modal title="Yeni Kategorei Ekle" open={isAddModalOpen} onCancel={() => setIsAddModalOpen(false)} footer={false}>
-                <Form layout="vertical" onFinish={onFinish} form={form}>
-                    <Form.Item name="title" label="Kategori Ekle" rules={[{ required: true, message: "Kategori Alanı Boş Geçilemez" }]}>
-                        <Input />
-                    </Form.Item>
-                    <Form.Item className="flex justify-end mb-0">
-                        <Button type="primary" htmlType="submit">Oluştur</Button>
-                    </Form.Item>
-                </Form>
-            </Modal>
+            <Add isAddModalOpen={isAddModalOpen} setIsAddModalOpen={setIsAddModalOpen} setCategories={setCategories} categories={categories} />
+
+            <li className="category-item !bg-orange-800 hover:opacity-90" onClick={() => setIsEditModalOpen(true)}>
+                <EditOutlined />
+            </li>
+            <Edit isEditModalOpen={isEditModalOpen} setIsEditModalOpen={setIsEditModalOpen} />
         </ul>
     )
 }
